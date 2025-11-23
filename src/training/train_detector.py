e12d96a5049586f12a72c18f116d49184745e3de
SergeyAkh <akhmadulinsergey@gmail.com>
2025-11-23 21:55:49 +0200

    new

--- src/training/train_detector.py
+++ src/training/train_detector.py
@@ -1,3 +1,3 @@
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from dataset.license_plate_dataset import LicensePlateDataset
+from src.dataset.license_plate_dataset import LicensePlateDataset
