a9f001aa1577cddd59bf77528ba2011dd5d45c90
SergeyAkh <akhmadulinsergey@gmail.com>
2025-11-23 22:13:38 +0200

    new

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-data_path = "/Users/sergeiakhmadulin/Plate_dataset"
+import os
+
+# Project root (resolved automatically)
+ROOT = "/Users/sergeiakhmadulin/Plate_dataset/3/YOLO_dataset"
+
+IMAGES_DIR = os.path.join(ROOT, "images")
+LABELS_DIR = os.path.join(ROOT, "labels")
--- src/training/train_detector.py
+++ src/training/train_detector.py
@@ -1,3 +1,25 @@
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from src.dataset.license_plate_dataset import LicensePlateDataset
+
+from config import IMAGES_DIR, LABELS_DIR
+
+def create_dataloader():
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_dataset = LicensePlateDataset(
+        img_dir=IMAGES_DIR,
+        label_dir=LABELS_DIR,
+        transforms=transform
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=8,
+        shuffle=True,
+        collate_fn=lambda x: tuple(zip(*x))
+    )
+
+    return train_loader
