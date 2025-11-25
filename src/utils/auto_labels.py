bca8325d8bc47321e72873af4d74b54c7b895daf
SergeyAkh <akhmadulinsergey@gmail.com>
2025-11-25 23:04:56 +0200

    new

--- src/utils/auto_labels.py
+++ src/utils/auto_labels.py
@@ -1,33 +1,30 @@
-from paddleocr import PaddleOCR
+import pytesseract
+from PIL import Image
 import os
 
-img_dir = "/Users/sergeiakhmadulin/Car_Number_Plate/predictions/crops"
-label_dir = "ModelB_dataset/train/labels"
+img_dir = "/Users/sergeiakhmadulin/Car_Number_Plate/predictions/crops/val/images"
+label_dir = "/Users/sergeiakhmadulin/Car_Number_Plate/predictions/crops/val/labels"
 
 os.makedirs(label_dir, exist_ok=True)
-
-ocr = PaddleOCR(use_angle_cls=True, lang="en")
 
 for img_name in os.listdir(img_dir):
     if not img_name.lower().endswith((".jpg", ".png")):
         continue
 
     img_path = os.path.join(img_dir, img_name)
-    result = ocr.ocr(img_path, cls=True)
 
+    img = Image.open(img_path)
+
+    config = "--oem 3 --psm 7 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+    text = pytesseract.image_to_string(img, config=config)
+    text = text.strip().replace(" ", "").replace("\n", "")
     # Extract text
-    raw_text = "".join([line[1][0] for line in result[0]])
 
-    # Normalize text: remove spaces, dashes, dots
-    clean_text = (
-        raw_text.replace(" ", "")
-                .replace("-", "")
-                .replace("_", "")
-    )
 
     # Save label
-    # label_path = os.path.join(label_dir, img_name.rsplit(".", 1)[0] + ".txt")
-    # with open(label_path, "w") as f:
-    #     f.write(clean_text)
+    label_path = os.path.join(label_dir, img_name.rsplit(".", 1)[0] + ".txt")
+    with open(label_path, "w") as f:
+        f.write(text)
 
-    print(img_name, "→", clean_text)
+    print(img_name, "→", text)
